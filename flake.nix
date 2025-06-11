0e90ef2669f8f4d3ace94831d681be8724e67d02
Will Brown <will@rebeagle.com>
2025-06-11 09:40:01 -0400

    added flake for nixos

---
+++ flake.nix
@@ -0,0 +1,63 @@
+rec {
+  description = "Ancient auto-clicker that still works.";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      system = "x86_64-linux"; # Or your desired system
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      packages.${system}.default = pkgs.stdenv.mkDerivation {
+        pname = "openergo";
+        version = "0.1.0"; # Or your desired version
+
+        src = builtins.path {
+          path = ./.;
+          name = "source";
+        };
+
+        nativeBuildInputs = [
+          pkgs.pkg-config
+          pkgs.qt5.qmake
+          pkgs.qt5.wrapQtAppsHook
+        ];
+
+        buildInputs = [
+          pkgs.qt5.qtbase
+          pkgs.qt5.qtmultimedia
+          pkgs.qt5.qtx11extras
+          pkgs.xorg.libXi
+          pkgs.xorg.libXtst
+          pkgs.xorg.libxcb
+        ];
+
+        installPhase = ''
+          runHook preInstall
+          mkdir -p $out/bin
+          install -m 755 openergo $out/bin/openergo
+          runHook postInstall
+        '';
+
+        meta = with pkgs.lib; {
+          inherit description;
+          homepage = "https://github.com/wpbrown/openergo";
+          license = licenses.gpl3;
+          platforms = platforms.linux;
+        };
+      };
+
+      apps.${system}.default = {
+        type = "app";
+        program = "${self.packages.${system}.default}/bin/openergo";
+      };
+
+      devShells.${system}.default = pkgs.mkShell {
+        inputsFrom = [ self.packages.${system}.default ];
+      };
+    };
+}
